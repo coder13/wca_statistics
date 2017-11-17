adb341b0cf39826e149fe8440cabc0d37ba47421
Caleb Hoover <choover11@gmail.com>
2017-11-17 07:54:01 -0800

    fixed home page

--- bin/compute_home.js
+++ bin/compute_home.js
@@ -4,7 +4,7 @@ const Path = require('path');
 const fs = require('fs');
 
 fs.readdir(Path.resolve('statistics'), (err, files) => {
-	let output = files.map(name => `- [${name}](${name})`).join('\n');
+	let output = files.map(name => name.replace(/\.js/, '')).map(name => `- [${name}](${name})`).join('\n');
 
 	fs.writeFile(Path.join('build/README.md'), output, function (err) {
 		if (err) throw err;
