631a7bede473616d9b455d6cd5ce7faf33e85f09
Caleb Hoover <choover11@gmail.com>
2017-11-17 08:07:01 -0800

    is this more appropriate to do?

--- bin/travis.sh
+++ bin/travis.sh
@@ -15,7 +15,7 @@ else
   else
     # Copy existing files from gh-pages to the build directory.
     git checkout gh-pages .
-    files_to_copy= `git ls-tree --name-only gh-pages | grep '.md'`
+    files_to_copy= $(git ls-tree --name-only gh-pages | grep '.md')
     if [[ "$files_to_copy" != "" ]]; then
       mv $files_to_copy build
     fi
