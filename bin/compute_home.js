24ac4eee17a6539af70bf0275bcc05a426460c3d
Caleb Hoover <choover11@gmail.com>
2017-11-17 14:24:30 -0800

    fixed compute_home.js

--- bin/compute_home.js
+++ bin/compute_home.js
@@ -9,7 +9,7 @@ fs.readdir(Path.resolve('statistics'), (err, files) => {
 		return `- [${statistic.title}](${name.replace(/\.js/, '')})`;
 	}).join('\n');
 
-	fs.writeFile(Path.join('build/README.md'), output, function (err) {
+	fs.writeFile(Path.join('build/README.md'), output, function (error) {
 		if (error) {throw error;}
 		console.log('Computed home page');
 	});
