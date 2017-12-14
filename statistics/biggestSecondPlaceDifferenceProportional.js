c112bf510c612f2547788ddaa9333e5a31b4c7ff
Caleb Hoover <choover11@gmail.com>
2017-12-14 13:44:30 -0800

    fixed biggestSecondPlaceDifference and added a proportional version

--- statistics/biggestSecondPlaceDifferenceProportional.js
+++ statistics/biggestSecondPlaceDifferenceProportional.js
@@ -5,11 +5,12 @@ const md = require('../core/markdown');
 const {timedEvents, eventNames, formatResult} = require('../core/wca.js');
 
 module.exports = {
-	title: 'Biggest Difference Between Second and First Place',
-	description: 'Biggest Difference Between Second and First Place',
+	title: 'Biggest Proportional Difference Between Second and First Place',
+	description: 'Biggest Proportional Difference Between Second and First Place',
 
 	query: (eventId) => `
 		SELECT
+			format.sort_by,
 			Competitions.id as competitionId,
 			Competitions.name as competitionName,
 			@first:= if(format.sort_by='average', F.average, F.best) as first,
@@ -18,13 +19,13 @@ module.exports = {
 			@second:= if(format.sort_by='average', S.average, S.best) as second,
 			S.personId as secondPersonId,
 			S.personName as secondPersonName,
-			@second - @first difference
+			TRUNCATE(@second / @first * 100, 2) proportionalDifference
 		FROM (SELECT competitionId, personId, personName, average, best FROM Results WHERE eventId='${eventId}' AND roundTypeId in ('c', 'f') AND pos = 1) F
 		JOIN (SELECT competitionId, personId, personName, average, best FROM Results WHERE eventId='${eventId}' AND roundTypeId in ('c', 'f') AND pos = 2) S ON F.competitionId = S.competitionId
 		JOIN preferred_formats pf ON pf.event_id = '${eventId}' AND ranking = 1
 		JOIN Formats format ON format.id = pf.format_id
 		JOIN Competitions ON F.competitionId = Competitions.id
-		ORDER BY Difference DESC
+		ORDER BY proportionalDifference DESC
 		LIMIT 15;
 	`,
 
@@ -42,16 +43,16 @@ module.exports = {
 				let lastValue;
 
 				let table = [['Place', 'Competition', 'First Place', 'Second Place', 'Difference']].concat(results.map(row => {
-					if (lastValue !== row.difference) {
+					if (lastValue !== row.proportionalDifference) {
 						place++;
-						lastValue = row.difference;
+						lastValue = row.proportionalDifference;
 					}
 
 					let competitionName = nameWithLinkToCompetitionId(row.competitionName, row.competitionId + `/results/all#e${eventId}_f`);
 					let first = `${formatResult(row.first)} (${nameWithLinkToWcaId(row.firstPersonName, row.firstPersonId)})`;
 					let second = `${formatResult(row.second)} (${nameWithLinkToWcaId(row.secondPersonName, row.secondPersonId)})`;
 
-					return [place, competitionName, first, second, formatResult(row.difference)];
+					return [place, competitionName, first, second, row.proportionalDifference + '%'];
 				}));
 
 				markdown += md.subHeader(eventNames[eventId], 3) + md.table(table);
