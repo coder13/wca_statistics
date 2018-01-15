1ae3db0a2aa33c55a5b4a030598ad0d752e17041
Caleb Hoover <choover11@gmail.com>
2018-01-14 19:09:53 -0800

    fixed queries, added single

--- statistics/longestStandingSinglePBs.js
+++ statistics/longestStandingSinglePBs.js
@@ -6,20 +6,20 @@ const md = require('../core/markdown');
 const {events, eventNames, formatResult} = require('../core/wca.js');
 
 module.exports = {
-	title: 'Longest Standing Average PBs',
+	title: 'Longest Standing Single PBs',
 
 	query: () => `
 		SELECT
-			CAR.personId,
+			CSR.personId,
 			personName,
-			CAR.eventId,
-			CAR.countryId,
-			CAR.average average,
+			CSR.eventId,
+			CSR.countryId,
+			CSR.best single,
 			DATE(CONCAT(year, '-', month, '-', day)) PBDate
-		FROM (SELECT MIN(valueAndId) % 1000000000 id FROM ConciseAverageResults GROUP BY personId,eventId) PBs
-		JOIN ConciseAverageResults CAR ON PBs.id = CAR.id 
+		FROM (SELECT MIN(valueAndId) % 1000000000 id FROM ConciseSingleResults GROUP BY personId,eventId) PBs
+		JOIN ConciseSingleResults CSR ON PBs.id = CSR.id 
 		JOIN (SELECT personId,personName,eventId, MAX(date) mostRecent FROM ResultDates GROUP BY personId,personName,eventId) recent
-			ON CAR.personId = recent.personId AND CAR.eventId = recent.eventId
+			ON CSR.personId = recent.personId AND CSR.eventId = recent.eventId
 		WHERE year(mostRecent) >= 2017
 		ORDER BY PBDate
 		LIMIT 100;
@@ -31,12 +31,12 @@ module.exports = {
 		query(self.query(), (error, results, fields) => {
 			if (error) {throw error;}
 
-			let table = [['Person', 'Event', 'Country', 'Average', 'Date Set']].concat(results.map(row => {
+			let table = [['Person', 'Event', 'Country', 'Single', 'Date Set']].concat(results.map(row => {
 				let person = nameWithLinkToWcaId(row.personName, row.personId);
 				let event = eventNames[row.eventId];
-				let average = formatResult(row.average, row.eventId, true);
+				let single = formatResult(row.single, row.eventId, false);
 
-				return [person, event, row.countryId, average, row.PBDate.toISOString().slice(0, 10)];
+				return [person, event, row.countryId, single, row.PBDate.toISOString().slice(0, 10)];
 			}));
 
 			let markdown = md.title(self.title) + md.table(table);
