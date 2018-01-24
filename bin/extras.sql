c1bdaca0083ac0277c60ac2845ca8c4ea1235a38
Caleb Hoover <choover11@gmail.com>
2018-01-24 08:33:00 -0800

    added ResultDates table

---
+++ bin/extras.sql
@@ -0,0 +1,36 @@
+START TRANSACTION;
+
+DROP FUNCTION IF EXISTS resultId;
+CREATE FUNCTION resultId(competitionId varchar(32), personId varchar(10), eventId varchar(6), roundTypeId char(1))
+RETURNS varchar(255) DETERMINISTIC
+RETURN CONCAT(competitionId, '-', personId, '-', eventId, '-', roundTypeId);
+
+## Attaches Dates and continent ids to results
+
+DROP TABLE IF EXISTS ResultDates;
+CREATE TABLE ResultDates AS (
+  SELECT resultId(R.competitionId, R.personId, R.eventId, R.roundTypeId) resultId,
+  R.personId, R.personName, R.countryId, Continents.recordName personContinentId,
+  R.competitionId, R.eventId, R.roundTypeId, R.formatId, R.pos, R.average, R.best, R.regionalAverageRecord, R.regionalSingleRecord,
+  @date := DATE(CONCAT(year, '-', month, '-', day)) date,
+  @weekend := DATE(DATE_SUB(@date, INTERVAL (DAYOFWEEK(@date) + 2) % 7 DAY)) weekend
+  FROM Results R
+  JOIN Competitions ON R.competitionId = Competitions.id
+  JOIN Countries ON Countries.id = R.countryId
+  LEFT JOIN Continents ON Countries.continentId = Continents.id
+);
+
+# ~1:30
+
+CREATE INDEX resultId ON ResultDates(resultId);
+CREATE INDEX eventRound ON ResultDates(eventId, roundTypeId);
+CREATE INDEX roundEventComp ON ResultDates(roundTypeId, eventId, competitionId);
+CREATE INDEX eventCountryDate ON ResultDates(eventId, personCountryId, date);
+CREATE INDEX eventContinentDate ON ResultDates(eventId, personContinentId, date);
+CREATE INDEX eventDateAvg ON ResultDates(eventId, date, average);
+
+DROP TABLE IF EXISTS W;
+DROP TABLE IF EXISTS C;
+DROP TABLE IF EXISTS N;
+
+COMMIT;
