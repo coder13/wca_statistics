95e02a51490bcdfbefce45b2ac94926d7339eebb
Caleb Hoover <choover11@gmail.com>
2018-01-25 09:33:45 -0800

    added progress to mostChampionships

--- statistics/mostChampionships.js
+++ statistics/mostChampionships.js
@@ -10,23 +10,23 @@ module.exports = {
 	query: (region) => `
 		SELECT
 			personId,
-			personName,
+			Persons.name,
 			COUNT(competitionId) count,
-			GROUP_CONCAT(competitionId ORDER BY year,month,day) competitions
+			GROUP_CONCAT(competitionId ORDER BY R.year, R.month, R.day) competitions
 		FROM (
 			SELECT
 				DISTINCT competitionId,
 				personId,
-				personName,
 				year,
 				month,
 				day
 			FROM Results
-			JOIN Competitions ON Results.competitionId = Competitions.id
+			JOIN Competitions C ON Results.competitionId = C.id
 		) R
 		RIGHT JOIN championships ON R.competitionId = championships.competition_id
-		WHERE championship_type = '${region}'
-		GROUP BY personId, personName
+		JOIN Persons ON R.personId=Persons.id
+		WHERE championship_type = '${region}' AND Persons.subid = 1
+		GROUP BY personId, Persons.name
 		ORDER BY count DESC LIMIT 25;
 	`,
 
@@ -36,6 +36,7 @@ module.exports = {
 		let markdown = md.title(this.title);
 
 		forEach(this.regions, function (region) {
+			console.log('Running for region: ', region[0]);
 			let done = this.async();
 
 			markdown += md.subHeader(region[0], 3);
@@ -44,7 +45,7 @@ module.exports = {
 				if (error) {throw error;}
 
 				let table = [['Person', 'Championships', 'Competitions']].concat(results.map(row => {
-					let person = nameWithLinkToWcaId(row.personName, row.personId);
+					let person = nameWithLinkToWcaId(row.name, row.personId);
 					let comps = row.competitions.split(',').map(comp => nameWithLinkToCompetitionId(comp, comp)).join(', ');
 
 					return [person, row.count, comps];
